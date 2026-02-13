91cdc723bda8677a0d46d40e33aafde8dd6687f1
ChicFlower <jay.phoenix.jjp@gmail.com>
2026-02-13 13:04:25 +0000

    ADDED TON OF STUFF

--- mainjavascript.js
+++ mainjavascript.js
@@ -8,8 +8,18 @@ function toggleTheme()
     }
     else
     {
-        theme.setAttribute('href', 'main-style-sheet-light.css')
+        theme.setAttribute('href', 'main-style-sheet-light.css');
     }
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "27vw";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
 // alert("hello, user");
