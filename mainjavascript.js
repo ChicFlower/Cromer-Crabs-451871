cdc24417f0030facda1b711a949dadbf9f52e247
ChicFlower <jay.phoenix.jjp@gmail.com>
2026-02-26 10:11:35 +0000

    added so much stuff

--- mainjavascript.js
+++ mainjavascript.js
@@ -1,24 +1,44 @@
 function toggleTheme()
 {
     let theme = document.getElementById('theme');
+    let hamburger_type = document.getElementById('sidebarnav');
 
     if (theme.getAttribute('href') == 'main-style-sheet-light.css')
     {
-        theme.setAttribute('href', 'main-style-sheet-dark.css');
+      theme.setAttribute('href', 'main-style-sheet-dark.css');
+      hamburger_type.setAttribute('src', 'hamburger_inverted.png');
+
     }
     else
     {
         theme.setAttribute('href', 'main-style-sheet-light.css');
+        hamburger_type.setAttribute('src', 'hamburger.png');
     }
 }
 
+let open = false;
+
 /* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "27vw";
+function openNav() 
+{
+  let mySidenav = document.getElementById("mySidenav");
+  if (open == false)
+  {
+    mySidenav.style.width = "27vw";
+    mySidenav.style.zIndex = "1";
+    open = true;
   }
+  else 
+  {
+    mySidenav.style.width="0";
+    open = false;
+    mySidenav.style.zIndex = "-1";
+  }
+}
   
   /* Set the width of the side navigation to 0 */
-function closeNav() {
+function closeNav() 
+{
   document.getElementById("mySidenav").style.width = "0";
 }
 
