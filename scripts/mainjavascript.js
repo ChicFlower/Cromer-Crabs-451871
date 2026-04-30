fc337b7efec4aca5137ee46aa461d1d5d1ce3595
ChicFlower <jay.phoenix.jjp@gmail.com>
2026-04-30 13:58:54 +0100

    omg fixes

--- scripts/mainjavascript.js
+++ scripts/mainjavascript.js
@@ -90,40 +90,40 @@ function openwebsiteTT()
   window.open("https://tiktok.com");
 }
 
-document.getElementById("rpic").addEventListener('click', function () 
-{
-  window.open("../pages/rhodri.html", "_self")
-});
+// document.getElementById("rpic").addEventListener('click', function () 
+// {
+//   window.open("../pages/rhodri.html", "_self");
+// });
 
-document.getElementById("mpic").addEventListener('click', function () 
-{
-  window.open("../pages/michael.html", "_self")
-});
+// document.getElementById("mpic").addEventListener('click', function () 
+// {
+//   window.open("../pages/michael.html", "_self");
+// });
 
-document.getElementById("dpic").addEventListener('click', function () 
-{
-  window.open("../pages/danny.html", "_self")
-});
+// document.getElementById("dpic").addEventListener('click', function () 
+// {
+//   window.open("../pages/danny.html", "_self");
+// });
 
-document.getElementById("arpic").addEventListener('click', function () 
-{
-  window.open("../pages/archie.html", "_self")
-});
+// document.getElementById("arpic").addEventListener('click', function () 
+// {
+//   window.open("../pages/archie.html", "_self");
+// });
 
-document.getElementById("aipic").addEventListener('click', function () 
-{
-  window.open("../pages/aiden.html", "_self")
-});
+// document.getElementById("aipic").addEventListener('click', function () 
+// {
+//   window.open("../pages/aiden.html", "_self");
+// });
 
-document.getElementById("ipic").addEventListener('click', function () 
-{
-  window.open("../pages/izzy.html", "_self")
-});
+// document.getElementById("ipic").addEventListener('click', function () 
+// {
+//   window.open("../pages/izzy.html", "_self");
+// });
 
-document.getElementById("tpic").addEventListener('click', function () 
-{
-  window.open("../pages/ty.html", "_self")
-});
+// document.getElementById("tpic").addEventListener('click', function () 
+// {
+//   window.open("../pages/ty.html", "_self");
+// });
 
 
 
---
+++ scripts/profileoverviewofteam.js
@@ -0,0 +1,34 @@
+document.getElementById("rpic").addEventListener('click', function () 
+{
+  window.open("../pages/rhodri.html", "_self");
+});
+
+document.getElementById("mpic").addEventListener('click', function () 
+{
+  window.open("../pages/michael.html", "_self");
+});
+
+document.getElementById("dpic").addEventListener('click', function () 
+{
+  window.open("../pages/danny.html", "_self");
+});
+
+document.getElementById("arpic").addEventListener('click', function () 
+{
+  window.open("../pages/archie.html", "_self");
+});
+
+document.getElementById("aipic").addEventListener('click', function () 
+{
+  window.open("../pages/aiden.html", "_self");
+});
+
+document.getElementById("ipic").addEventListener('click', function () 
+{
+  window.open("../pages/izzy.html", "_self");
+});
+
+document.getElementById("tpic").addEventListener('click', function () 
+{
+  window.open("../pages/ty.html", "_self");
+});
