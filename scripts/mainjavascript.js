92ff6475498f5500439548ee5d72acd275545a00
ChicFlower <jay.phoenix.jjp@gmail.com>
2026-04-30 11:59:58 +0100

    added stuff

--- scripts/mainjavascript.js
+++ scripts/mainjavascript.js
@@ -90,6 +90,44 @@ function openwebsiteTT()
   window.open("https://tiktok.com");
 }
 
+document.getElementById("rpic").addEventListener('click', function () 
+{
+  window.open("../pages/rhodri.html", "_self")
+});
+
+document.getElementById("mpic").addEventListener('click', function () 
+{
+  window.open("../pages/michael.html", "_self")
+});
+
+document.getElementById("dpic").addEventListener('click', function () 
+{
+  window.open("../pages/danny.html", "_self")
+});
+
+document.getElementById("arpic").addEventListener('click', function () 
+{
+  window.open("../pages/archie.html", "_self")
+});
+
+document.getElementById("aipic").addEventListener('click', function () 
+{
+  window.open("../pages/aiden.html", "_self")
+});
+
+document.getElementById("ipic").addEventListener('click', function () 
+{
+  window.open("../pages/izzy.html", "_self")
+});
+
+document.getElementById("tpic").addEventListener('click', function () 
+{
+  window.open("../pages/ty.html", "_self")
+});
+
+
+
+
 var slideIndex = 1;
 var slideIndex2 = 1;
 showDivs(slideIndex);
@@ -134,5 +172,3 @@ function showDivs2(n)
   }
   x[slideIndex2-1].style.display="block";
 }
-
-
