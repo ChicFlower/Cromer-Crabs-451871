7bb918e4c4f1e66890e83ec01270dae1f9afb29b
ChicFlower <jay.phoenix.jjp@gmail.com>
2026-05-01 09:36:35 +0100

    created all sections for individual players

--- scripts/mainjavascript.js
+++ scripts/mainjavascript.js
@@ -90,44 +90,6 @@ function openwebsiteTT()
   window.open("https://tiktok.com");
 }
 
-// document.getElementById("rpic").addEventListener('click', function () 
-// {
-//   window.open("../pages/rhodri.html", "_self");
-// });
-
-// document.getElementById("mpic").addEventListener('click', function () 
-// {
-//   window.open("../pages/michael.html", "_self");
-// });
-
-// document.getElementById("dpic").addEventListener('click', function () 
-// {
-//   window.open("../pages/danny.html", "_self");
-// });
-
-// document.getElementById("arpic").addEventListener('click', function () 
-// {
-//   window.open("../pages/archie.html", "_self");
-// });
-
-// document.getElementById("aipic").addEventListener('click', function () 
-// {
-//   window.open("../pages/aiden.html", "_self");
-// });
-
-// document.getElementById("ipic").addEventListener('click', function () 
-// {
-//   window.open("../pages/izzy.html", "_self");
-// });
-
-// document.getElementById("tpic").addEventListener('click', function () 
-// {
-//   window.open("../pages/ty.html", "_self");
-// });
-
-
-
-
 var slideIndex = 1;
 var slideIndex2 = 1;
 showDivs(slideIndex);
